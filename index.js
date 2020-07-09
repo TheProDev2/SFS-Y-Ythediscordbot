67d6feda714a644808f2ba3928ecd67af568fe18
TheProMusic <vinay.ory@gmail.com>
2020-07-09 14:43:23 +0300

    First

--- index.js
+++ index.js
@@ -1,244 +1,314 @@
-const Discord = require('discord.js');
-const { Client, Attachment } = require('discord.js');
+const Discord = require("discord.js");
+const { Client, Attachment, MessageEmbed } = require("discord.js");
 const bot = new Discord.Client();
 
-const ms = require('ms')
+const ms = require("ms");
 
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
-const request = require('request');
+const request = require("request");
 
-const PREFIX = '&';
+const PREFIX = "&";
 
-var version = '1.1.0';
+var version = "1.1.1";
 
 const serverStats = {
-    serverID: '729338182528663714',
-    totalUsersID: '730343531650220065',
-    memberCountID: '730343640206934047',
-    botCountID: '730343681751646209'
+  serverID: "729338182528663714",
+  totalUsersID: "730343531650220065",
+  memberCountID: "730343640206934047",
+  botCountID: "730343681751646209",
 };
 
-bot.on('guildMemberAdd', member => {
-    if(member.guild.id !== serverStats.serverID) return;
+bot.on("guildMemberAdd", (member) => {
+  if (member.guild.id !== serverStats.serverID) return;
 
-    bot.channels.cache.get(serverStats.totalUsersID).setName(`Total Users: ${member.guild.memberCount}`);
-    bot.channels.cache.get(serverStats.memberCountID).setName(`Members Count: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
-    bot.channels.cache.get(serverStats.botCountID).setName(`Bot Count: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+  bot.channels.cache
+    .get(serverStats.totalUsersID)
+    .setName(`Total Users: ${member.guild.memberCount}`);
+  bot.channels.cache
+    .get(serverStats.memberCountID)
+    .setName(
+      `Members Count: ${
+        member.guild.members.cache.filter((m) => !m.user.bot).size
+      }`
+    );
+  bot.channels.cache
+    .get(serverStats.botCountID)
+    .setName(
+      `Bot Count: ${member.guild.members.cache.filter((m) => m.user.bot).size}`
+    );
 });
 
-bot.on('guildMemberRemove', member => {
-    if(member.guild.id !== serverStats.serverID) return;
+bot.on("guildMemberRemove", (member) => {
+  if (member.guild.id !== serverStats.serverID) return;
 
-    bot.channels.cache.get(serverStats.totalUsersID).setName(`Total Users: ${member.guild.memberCount}`);
-    bot.channels.cache.get(serverStats.memberCountID).setName(`Members Count: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
-    bot.channels.cache.get(serverStats.botCountID).setName(`Bot Count: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+  bot.channels.cache
+    .get(serverStats.totalUsersID)
+    .setName(`Total Users: ${member.guild.memberCount}`);
+  bot.channels.cache
+    .get(serverStats.memberCountID)
+    .setName(
+      `Members Count: ${
+        member.guild.members.cache.filter((m) => !m.user.bot).size
+      }`
+    );
+  bot.channels.cache
+    .get(serverStats.botCountID)
+    .setName(
+      `Bot Count: ${member.guild.members.cache.filter((m) => m.user.bot).size}`
+    );
 });
 
-bot.on('ready', () => {
-    console.log("SFS Y-Y bot is online!");
-})
-
-bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-    if (!channel) return;
-
-    channel.send(`Welcome to our server, ${member}, please read the rules in the rules channel`);
+bot.on("ready", () => {
+  console.log("SFS Y-Y bot is online!");
 });
 
-bot.on('message', msg => {
-    if (msg.content === 'Hi') {
-        msg.reply('Hello!');
+bot.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (channel) => channel.name === "welcome"
+  );
+  if (!channel) return;
+
+  channel.send(
+    `Welcome to our server, ${member}, please read the rules in the rules channel`
+  );
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "Hi") {
+    msg.reply("Hello!");
+  }
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "Hello") {
+    msg.reply("Hello!");
+  }
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "שלום") {
+    msg.reply("שלום!");
+  }
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "היי") {
+    msg.reply("שלום!");
+  }
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "מה קורה") {
+    msg.reply("הכל בסדר!");
+  }
+});
+
+bot.on("message", (msg) => {
+  if (msg.content === "ping" || msg.content === "Ping") {
+    msg.reply("pong!");
+  }
+});
+
+bot.on("message", (message) => {
+  if (message.author.bot) return;
+  if (
+    message.content.toLowerCase() === "&verify" &&
+    message.channel.id === "729994672847454218"
+  ) {
+    let role = message.guild.roles.cache.get("729597571156148274");
+
+    if (role) {
+      try {
+        message.member.roles.add(role);
+        message.channel.send(`${message.author.username} is verified!`);
+        console.log("Role added!");
+      } catch (err) {
+        console.log(err);
+      }
     }
-})
+  }
+});
 
-bot.on('message', msg => {
-    if (msg.content === 'Hello') {
-        msg.reply('Hello!');
-    }
-})
+bot.on("message", (msg) => {
+  let args = msg.content.substring(PREFIX.length).split(" ");
 
-bot.on('message', msg => {
-    if (msg.content === 'שלום') {
-        msg.reply('שלום!');
-    }
-})
+  switch (args[0]) {
+    case "clear":
+      if (!msg.member.roles.cache.find((r) => r.name === "Staff"))
+        return msg.channel.send(
+          "You dont have the permisions to do this command."
+        );
+      if (!args[1]) return msg.reply("Error, please define second arg");
+      msg.channel.bulkDelete(args[1]);
+      break;
 
-bot.on('message', msg => {
-    if (msg.content === 'היי') {
-        msg.reply('שלום!');
-    }
-})
+    case "help":
+      const helperEmbed = new Discord.MessageEmbed()
+        .setColor("#ff7600")
+        .setAuthor("SFS Y-Y")
+        .setTitle("Helper Embed")
+        .setThumbnail(
+          "https://yt3.ggpht.com/a/AATXAJw8yiJdcR7B8ddAlRorfp8VzeTPuf_pKy3kzRFw=s144-c-k-c0xffffffff-no-rj-mo"
+        )
+        .setDescription(
+          "You can do this command only in SFS Y-Y official server."
+        )
+        .addFields(
+          { name: "Prefix", value: "The bot prefix is: &" },
+          {
+            name: "Commands",
+            value:
+              "The commands are in #bot-commands-info in SFS Y-Y official server",
+          }
+        )
+        .setTimestamp()
+        .setFooter(
+          "SFS Y-Y the official discord bot",
+          "https://yt3.ggpht.com/a/AATXAJw8yiJdcR7B8ddAlRorfp8VzeTPuf_pKy3kzRFw=s144-c-k-c0xffffffff-no-rj-mo"
+        );
 
-bot.on('message', msg => {
-    if (msg.content === 'מה קורה') {
-        msg.reply('הכל בסדר!');
-    }
-})
+      msg.author.send(helperEmbed);
+      break;
 
-bot.on('message', msg => {
-    if (msg.content === 'ping' || msg.content === 'Ping') {
-        msg.reply('pong!');
-    }
-})
+    case "joke":
+      msg.reply(
+        'A man is talking to God. The man: "God, how long is a million years?" God: "To me it is about a minute." The Man: "God, how much is a million dollars?" God: "To me it is a penny" The man: "God, may I have a penny?" God: "Wait a minute."'
+      );
+      break;
+  }
+});
 
-bot.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.toLowerCase() === '&verify' && message.channel.id === '729994672847454218') {
-        let role = message.guild.roles.cache.get('729597571156148274');
+bot.on("message", (message) => {
+  let args = message.content.substring(PREFIX.length).split(" ");
 
-        if (role) {
-            try {
-                message.member.roles.add(role)
-                message.channel.send(`${message.author.username} is verified!`);
-                console.log("Role added!");
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
-    }
-})
+  switch (args[0]) {
+    case "poll":
+      if (!msg.member.roles.cache.find((r) => r.name === "Staff"))
+        return msg.channel.send(
+          "You dont have the permisions to do this command."
+        );
 
-bot.on('message', msg => {
-    let args = msg.content.substring(PREFIX.length).split(" ");
+      const Embed = new Discord.MessageEmbed()
+        .setColor(0x00bdff)
+        .setTitle("Initiate Poll")
+        .setDescription("v!poll to initiate a simple yes or no poll");
 
-    switch (args[0]) {
-        case 'clear':
-            if (!msg.member.roles.cache.find(r => r.name === "Staff")) return msg.channel.send('You dont have the permisions to do this command.')
-            if (!args[1]) return msg.reply("Error, please define second arg")
-            msg.channel.bulkDelete(args[1]);
-            break;
+      if (!args[1]) {
+        message.channel.send(Embed);
+      }
 
-        case 'joke':
-            msg.reply('A man is talking to God. The man: "God, how long is a million years?" God: "To me it is about a minute." The Man: "God, how much is a million dollars?" God: "To me it is a penny" The man: "God, may I have a penny?" God: "Wait a minute."');
-            break;
-    }
-})
+      let msgArgs = args.slice(1).join(" ");
 
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
+      message.channel
+        .send("📋 " + "**" + msgArgs + "**")
+        .then((messageReaction) => {
+          messageReaction.react("👍");
+          messageReaction.react("👎");
+          message.delete({ timeout: 1000 }).catch(console.error);
+        });
 
-    switch (args[0]) {
+      break;
 
-        case 'poll':
+    case "kick":
+      if (!msg.member.roles.cache.find((r) => r.name === "Staff"))
+        return msg.channel.send(
+          "You dont have the permisions to do this command."
+        );
 
-            if (!msg.member.roles.cache.find(r => r.name === "Staff")) return msg.channel.send('You dont have the permisions to do this command.')
+      const user = message.mentions.users.first();
 
-            const Embed = new Discord.MessageEmbed()
-                .setColor(0x00BDFF)
-                .setTitle("Initiate Poll")
-                .setDescription("v!poll to initiate a simple yes or no poll")
+      if (user) {
+        const member = message.guild.member(user);
 
-            if (!args[1]) {
-                message.channel.send(Embed);
-            }
-
-            let msgArgs = args.slice(1).join(" ");
-
-            message.channel.send("📋 " + "**" + msgArgs + "**").then(messageReaction => {
-                messageReaction.react("👍");
-                messageReaction.react("👎");
-                message.delete({ timeout: 1000 }).catch(console.error);
+        if (member) {
+          member
+            .kick("You where kick for trolling!")
+            .then(() => {
+              message.reply(`Sucessfully kicked ${user.tag}`);
+            })
+            .catch((err) => {
+              message.reply(`I was unbale to kick the member`);
+              console.log(err);
             });
+        } else {
+          message.reply("That user isn't in this guild");
+        }
+      } else {
+        message.reply("You need to specify a person!");
+      }
 
-            break;
+      break;
 
-        case 'kick':
+    case "ban":
+      if (!msg.member.roles.cache.find((r) => r.name === "Staff"))
+        return msg.channel.send(
+          "You dont have the permisions to do this command."
+        );
 
-            if (!msg.member.roles.cache.find(r => r.name === "Staff")) return msg.channel.send('You dont have the permisions to do this command.')
+      const userman = message.mentions.users.first();
 
-            const user = message.mentions.users.first();
+      if (userman) {
+        const member = message.guild.member(userman);
 
-            if (user) {
-                const member = message.guild.member(user);
+        if (member) {
+          member
+            .ban({ ressino: "You ware bad!" })
+            .then(() => {
+              message.reply(`We banned the player: ${userman.tag}`);
+            })
+            .catch((err) => {
+              message.reply(`I was unbale to ban the member`);
+              console.log(err);
+            });
+        } else {
+          message.reply("That user isn't in this guild");
+        }
+      } else {
+        message.reply("You need to specify a person!");
+      }
 
-                if (member) {
-                    member.kick('You where kick for trolling!').then(() => {
-                        message.reply(`Sucessfully kicked ${user.tag}`);
-                    }).catch(err => {
-                        message.reply(`I was unbale to kick the member`);
-                        console.log(err);
-                    });
-                } else {
-                    message.reply("That user isn't in this guild");
-                }
-            } else {
-                message.reply("You need to specify a person!")
-            }
+      break;
 
-            break;
+    case "image":
+      image(message);
 
-        case 'ban':
-
-            if (!msg.member.roles.cache.find(r => r.name === "Staff")) return msg.channel.send('You dont have the permisions to do this command.')
-
-            const userman = message.mentions.users.first();
-
-            if (userman) {
-                const member = message.guild.member(userman);
-
-                if (member) {
-                    member.ban({ ressino: 'You ware bad!' }).then(() => {
-                        message.reply(`We banned the player: ${userman.tag}`);
-                    }).catch(err => {
-                        message.reply(`I was unbale to ban the member`);
-                        console.log(err);
-                    });
-                } else {
-                    message.reply("That user isn't in this guild");
-                }
-            } else {
-                message.reply("You need to specify a person!")
-            }
-
-            break;
-
-        case 'image':
-            image(message);
-
-            break
-    }
+      break;
+  }
 });
 
 function image(message) {
+  var options = {
+    url: "http://results.dogpile.com/serp?qc=images&q=" + "rocket",
+    method: "GET",
+    headers: {
+      Accept: "text/html",
+      "User-Agent": "Chrome",
+    },
+  };
 
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "rocket",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
+  request(options, function (error, response, responseBody) {
+    if (error) {
+      return;
+    }
 
+    $ = cheerio.load(responseBody);
 
+    var links = $(".image a.link");
 
+    var urls = new Array(links.length)
+      .fill(0)
+      .map((v, i) => links.eq(i).attr("href"));
 
+    console.log(urls);
 
-    request(options, function (error, response, responseBody) {
-        if (error) {
-            return;
-        }
+    if (!urls.length) {
+      return;
+    }
 
-
-        $ = cheerio.load(responseBody);
-
-
-        var links = $(".image a.link");
-
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-
-        console.log(urls);
-
-        if (!urls.length) {
-
-            return;
-        }
-
-        // Send result
-        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
-    });
+    // Send result
+    message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+  });
 }
 
 bot.login(process.env.token);

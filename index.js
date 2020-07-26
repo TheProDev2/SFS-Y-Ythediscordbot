a594d0916a37f823b2055f02dae48f4e088f2661
TheProMusic <vinay.ory@gmail.com>
2020-07-26 10:09:07 +0300

    bot

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const Discord = require("discord.js");
 const { Client, Attachment, MessageEmbed } = require("discord.js");
 const bot = new Discord.Client();
 
+bot.commands = new Discord.Collection();
+
+const fs = require("fs");
+
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  bot.commands.set(command.name, command);
+}
+
 const ms = require("ms");
 
 const cheerio = require("cheerio");
@@ -267,30 +281,8 @@ bot.on("message", (message) => {
 
 bot.on("message", (message) => {
   let args = message.content.substring(PREFIX.length).split(" ");
-  switch (args[0]) {
-    case "DM":
-      if (!message.member.roles.cache.find((r) => r.name === "Admin"))
-        return message.reply(
-          "You dont have the permisions to use this command."
-        );
 
-      let user =
-        message.mentions.users.first() ||
-        message.guild.members.cache.get(args[0]);
-
-      if (!user)
-        return message.reply(
-          "You did not @mention a user, or you gave an invalid ID."
-        );
-      if (!args.slice(1).join(" "))
-        return message.reply("You did not specify your message!");
-
-      user
-        .send(args.slice(1).join(" "))
-        .catch(() => message.channel.send("That user could not be a DMed!"))
-        .then(`Sent a message to ${user.tag}`);
-      break;
-  }
+  bot.commands.get("DM").execute(message, args);
 });
 
 /* ------------------------------------------------------------------------------------------------------- */
